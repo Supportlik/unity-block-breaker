8d5cf22a6968af663413d6a9962f34a15f773b4b
Michael Bortlik <Michael.Bortlik@gmail.com>
2015-11-29 19:18:59 +0100

    Sprites + Music done

---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void LoadLevel(string name){
+		Debug.Log("Trying to load: " + name);
+		Application.LoadLevel(name);	
+	}	
+	
+	public void Quit(){
+		Debug.Log("QUIT!");
+		Application.Quit();
+	}
+}
---
+++ Assets/_Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	public static MusicPlayer instance = null;
+	
+	// Use this for initialization
+	void Start () {
+		if(instance == null){
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy (gameObject);
+			Debug.Log("Destroy duplicate music player!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
